ff9e1ac3c3cb72de735e3dfc641faa54caa68898
Matt Johnson <matt@packettide.com>
2021-01-13 12:56:18 -0500

    Added addon.setup

--- low_title/addon.setup.php
+++ low_title/addon.setup.php
@@ -1,20 +1,15 @@
 <?php
 
-/**
- * Low Title config file
- *
- * @package        low_title
- * @author         Lodewijk Schutte <hi@gotolow.com>
- * @link           http://gotolow.com/addons/low-title
- * @copyright      Copyright (c) 2015, Low
- */
+require_once 'autoload.php';
+$addonJson = json_decode(file_get_contents(__DIR__ . '/addon.json'));
 
 return array(
-    'author'      => 'Low',
-    'author_url'  => 'http://gotolow.com/',
-    'docs_url'    => 'http://gotolow.com/addons/low-title',
-    'name'        => 'Low Title',
-    'description' => 'Quickly retrieve a title from an entry, category, channel or site',
-    'version'     => '3.0.0',
-    'namespace'   => 'Low\Title'
+    'name'              => $addonJson->name,
+    'description'       => $addonJson->description,
+    'version'           => $addonJson->version,
+    'namespace'         => $addonJson->namespace,
+    'author'            => 'EEHarbor',
+    'author_url'        => 'http://eeharbor.com/low_title',
+    'docs_url'          => 'http://eeharbor.com/low_title/documentation',
+    'settings_exist'    => false,
 );
